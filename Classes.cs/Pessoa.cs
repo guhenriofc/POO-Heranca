b74f4b42778a97c0c16ceb097c99438b8ce552d2
guhenriofc <you@example.com>
2020-12-08 14:12:57 -0200

    POO-Heranca  realizado

---
+++ Classes.cs/Pessoa.cs
@@ -0,0 +1,10 @@
+namespace POO_Heranca.Classes.cs
+{
+    public class Pessoa
+    {
+        public string nome;
+        public string DarBoasVindas(string nomeUsuario){
+            return "Olá "+nomeUsuario+"! Seja Bem Vindo(a) ao nossa sistema";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using POO_Heranca.Classes.cs;
+
+namespace POO_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaFisica pf = new PessoaFisica();
+            pf.cpf = "000.000.000-00";
+            pf.nome = "Gustavo";
+            Console.WriteLine(pf.DarBoasVindas(pf.nome));
+            
+        }
+    }
+}
